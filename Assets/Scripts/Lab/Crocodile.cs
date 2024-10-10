52550ec3f508393b679b797437387fb3e091fc00
matthew <sorawit.wann@bumail.net>
2024-10-10 17:53:51 +0700

    Add Crocodile Shoot the Rok

--- Assets/Scripts/Lab/Crocodile.cs
+++ Assets/Scripts/Lab/Crocodile.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class Crocodile : Enemy
 {
-    private float attackRange;
-    private Player player;
+    [SerializeField] private float attackRange;
+    [SerializeField] private Player player;
+
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private Transform bulletSpawnPoint;
+    [SerializeField] private float bulletWaitTime;
+    [SerializeField] private float bulletTimer;
+   
 
     private void Start()
     {
-        Init(100);
-        Debug.Log("Crocodile " + Health);
+        Init(100);       
+    }
+
+    private void Update()
+    { 
+        bulletTimer -= Time.deltaTime;
 
         Behaviour();
+
+        if(bulletTimer < 0)
+        {
+            bulletTimer = bulletWaitTime;
+        }
     }
 
     public override void Behaviour()
     {
-        Debug.Log("Crocodile.Behaviour");
+        Vector2 direction = player.transform.position - transform.position;
+        float distance = direction.magnitude;
+
+        if (distance < attackRange)
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
     }
 
     
